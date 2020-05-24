9210ff691346f25be9740f0f3f23fc5b8f7c922c
Christopher Brantley <c_brantl@uncg.edu>
2020-05-24 01:37:28 -0400

    Functions added for classifying political posts between cities and for retrieving top defining vocabulary.

--- Bayes/bayes.py
+++ Bayes/bayes.py
@@ -4,6 +4,8 @@
 
 from numpy import *
 import re
+import operator
+import feedparser
 
 def loadDataSet():
     postingList = [["my", "dog", "has", "flea", \
@@ -95,7 +97,7 @@ def spamTest():
     
     # Application of Hold-out cross validation.
     for i in range(10):
-        randIndex = int(random.uniform(0, len(trainingSet)))
+        randIndex = int(random.uniform(0, 5))
         testSet.append(trainingSet[randIndex])
         del(trainingSet[randIndex])
     trainMat = []
@@ -103,7 +105,7 @@ def spamTest():
     
     # Training.
     for docIndex in trainingSet:
-        trainMat.append(setOfWords2Vec(vocabList, \
+        trainMat.append(bagOfWords2VecMN(vocabList, \
                                        docList[docIndex]))
         trainClasses.append(classList[docIndex])
     p0V, p1V, pSpam = trainNB0(array(trainMat), \
@@ -112,14 +114,101 @@ def spamTest():
     
     # Testing.
     for docIndex in testSet:
-        wordVector = setOfWords2Vec(vocabList, \
+        wordVector = bagOfWords2VecMN(vocabList, \
                                     docList[docIndex])
         if classifyNB(array(wordVector), p0V, p1V, pSpam) \
            != classList[docIndex]:
             errorCount += 1
+            print("Classification Error: ", docList[docIndex])
     print( "The error rate is: ", float(errorCount) / \
                                        len(testSet))
 
+def calcMostFreq(_vocabList, _fullText):
+    freqDict = {}
+    for token in _vocabList:
+        freqDict[token] = _fullText.count(token)
+    sortedFreq = sorted(freqDict.items(), \
+                        # Sort by first item.
+                        key = operator.itemgetter(1), \
+                        reverse = True)
+    return sortedFreq[:30]
+
+def localWords(_feed1, _feed0):
+    docList = []
+    classList = []
+    fullText = []
+    minLen = min(len(_feed1['entries']), len(_feed0['entries']))
+    for i in range(minLen):
+        wordList = textParse(_feed1['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(1) # 1 -> houston
+        wordList = textParse(_feed0['entries'][i]['summary'])
+        docList.append(wordList)
+        fullText.extend(wordList)
+        classList.append(0) # 0 -> New York
+    vocabList = createVocabList(docList)
+    top30Words = calcMostFreq(vocabList, fullText)
+    for pairW in top30Words:
+        if pairW[0] in vocabList:
+            vocabList.remove(pairW[0])
+    trainingSet = list(range(2 * minLen))
+    testSet = []
+    for i in range(20):
+        randIndex = int(random.uniform(0, len(trainingSet)))
+        testSet.append(trainingSet[randIndex])
+        del(trainingSet[randIndex])
+    trainMat = []
+    trainClasses = []
+    for docIndex in trainingSet:
+        trainMat.append(bagOfWords2VecMN(vocabList, \
+                                         docList[docIndex]))
+        trainClasses.append(classList[docIndex])
+    p0V, p1V, pSpam = trainNB0(array(trainMat), \
+                               array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:
+        wordVector = bagOfWords2VecMN(vocabList, \
+                                      docList[docIndex])
+        if classifyNB(array(wordVector), p0V, p1V, pSpam) \
+           != classList[docIndex]:
+            errorCount += 1
+    print("The error rate is: ", float(errorCount) / len(testSet))
+    return vocabList, p0V, p1V
+
+def getTopWords(_houston, _newYork):
+    vocabList, p0V, p1V = localWords(_houston, _newYork)
+    tophouston = []
+    topNewYork = []
+    # 1 -> houston
+    # 0 -> New York
+    for i in range(len(p0V)):
+        if p0V[i] > -6.0:
+            tophouston.append((vocabList[i], p0V[i]))
+        if p1V[i] > -6.0:
+            topNewYork.append((vocabList[i], p1V[i]))
+    sortedhouston = sorted(tophouston, \
+                          key = lambda pair: pair[1], \
+                          reverse = True)
+    sortedNewYork = sorted(topNewYork, \
+                           key = lambda pair: pair[1], \
+                           reverse = True)
+    print("\nhouston Top Words")
+    count = 0
+    for item in sortedhouston:
+        print(item[0])
+        if count == 10:
+            break
+        count += 1
+
+    count = 0 
+    print("\nNew York Top Words")
+    for item in sortedNewYork:
+        print(item[0])
+        if count == 10:
+            break
+        count += 1
+        
 def main():
     posts, labels = loadDataSet()
     vocabList = createVocabList(posts)
@@ -137,7 +226,16 @@ def main():
           classifyNB(testVec, p0V, p1V, pAB), "\n")
 
     print("Spam Email Classifier Error Rate")
-    for x in range(10):
+    for x in range(5):
         spamTest()
-        
+
+    # RSS feed analysis of political identity.
+    print("\n\nRSS feed analysis of political identity.")
+    houstonRSS = \
+    "https://houston.craigslist.org/search/pol?format=rss"
+    newYorkRSS = \
+    "https://newyork.craigslist.org/search/pol?format=rss"
+    houston = feedparser.parse(houstonRSS)
+    newYork = feedparser.parse(newYorkRSS)
+    getTopWords(houston, newYork)
 main()
